537710912a4c8db98e53b7e445b8f3beef920ff5
Criis969 <cristian.sanz@estudiantat.upc.edu>
2021-12-17 16:35:56 +0100

    Maps, digitales

---
+++ lib/generated/intl/messages_es_ES.dart
@@ -0,0 +1,44 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a es_ES locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'es_ES';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "allExercices": MessageLookupByLibrary.simpleMessage("Publicaciones"),
+        "chat": MessageLookupByLibrary.simpleMessage("Chat"),
+        "email": MessageLookupByLibrary.simpleMessage(
+            "Introducir correo electronico"),
+        "hello": MessageLookupByLibrary.simpleMessage("Hola"),
+        "login": MessageLookupByLibrary.simpleMessage("Iniciar Sesión"),
+        "maps": MessageLookupByLibrary.simpleMessage("Mapa"),
+        "password":
+            MessageLookupByLibrary.simpleMessage("Introducir contraseña"),
+        "register": MessageLookupByLibrary.simpleMessage("Registrarse"),
+        "report": MessageLookupByLibrary.simpleMessage("Reportar"),
+        "search": MessageLookupByLibrary.simpleMessage("Buscar"),
+        "settings": MessageLookupByLibrary.simpleMessage("Ajustes"),
+        "time": MessageLookupByLibrary.simpleMessage("Todo a tiempo"),
+        "today": MessageLookupByLibrary.simpleMessage("Calendario"),
+        "user": MessageLookupByLibrary.simpleMessage("Nombre de usuario"),
+        "wrongPass":
+            MessageLookupByLibrary.simpleMessage("¿Has olvidado la contraseña?")
+      };
+}
---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:provider/provider.dart';
+import 'package:registro/screens/MapaScreen.dart';
+import 'package:registro/screens/splash_screen.dart';
+import 'blocs/registro_bloc.dart';
+import 'generated/l10n.dart';
+import 'screens/registro.dart';
+import 'screens/LoginPage.dart';
+import 'screens/home_page.dart';
+import 'screens/welcome_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    S.load(Locale('es', 'ES'));
+
+    return Provider(
+      create: (context) => RegistroBloc(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'inTime',
+        theme: ThemeData(
+          primarySwatch: Colors.orange,
+        ),
+        localizationsDelegates: [
+          S.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: S.delegate.supportedLocales,
+        
+        home: const SplashScreen(),
+        routes: routes,
+      ),
+    );
+  }
+}
+
+var routes = <String,WidgetBuilder>{
+  "/welcome":(BuildContext context) => const WelcomeScreen(),
+  "/login":(BuildContext context) => LoginPage(), //Poner el nombre de la clase de login de su propia pantalla
+  "/register":(BuildContext context) => Registro(),
+  "/home":(BuildContext context) => HomeScreen(), //Poner el nombre de la clase de register de su propia pantalla
+  "/map":(BuildContext context) => MapaScreen(), //Poner el nombre de la clase de register de su propia pantalla
+};
+
